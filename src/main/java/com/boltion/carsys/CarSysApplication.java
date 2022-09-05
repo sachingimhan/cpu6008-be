166518a9cc3f46991f69019123d55e91e57f4936
sachingimhan <sachingimhan8@gmail.com>
2022-09-05 10:03:43 +0530

    init commit

---
+++ src/main/java/com/boltion/carsys/CarSysApplication.java
@@ -0,0 +1,13 @@
+package com.boltion.carsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSysApplication.class, args);
+    }
+
+}
