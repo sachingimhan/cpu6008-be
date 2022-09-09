d16afa3e82c1c888e41560fbc60d19d651fe8f6a
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2022-09-09 06:21:35 +0530

    user login dto update

--- src/main/java/com/boltion/carsys/dto/UserLoginDTO.java
+++ src/main/java/com/boltion/carsys/dto/UserLoginDTO.java
@@ -6,11 +6,8 @@ import lombok.*;
  * @auther sachin
  * @date 2022-09-05
  */
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
+
+@Data
 public class UserLoginDTO {
     private String id;
     private String email;
@@ -19,11 +16,75 @@ public class UserLoginDTO {
     private CustomerDTO customer;
     private DriverDTO driver;
 
-    public UserLoginDTO(String id, String email, String password, String role, CustomerDTO customer) {
+    public UserLoginDTO() {
+    }
+
+    public UserLoginDTO(String id, String email, String password, String role, CustomerDTO customer, DriverDTO driver) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.role = role;
         this.customer = customer;
+        this.driver = driver;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    public DriverDTO getDriver() {
+        return driver;
+    }
+
+    public void setDriver(DriverDTO driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLoginDTO{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", customer=" + customer +
+                ", driver=" + driver +
+                '}';
     }
 }
