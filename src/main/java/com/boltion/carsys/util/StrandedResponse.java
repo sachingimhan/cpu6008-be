376dd1b47724f6e0c340d27938697f99d661f74e
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2022-09-09 06:31:44 +0530

    Update StrandedResponse.java

--- src/main/java/com/boltion/carsys/util/StrandedResponse.java
+++ src/main/java/com/boltion/carsys/util/StrandedResponse.java
@@ -1,23 +1,56 @@
 package com.boltion.carsys.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @auther sachin
  * @date 2022-09-05
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class StrandedResponse {
     private boolean state;
     private String message;
     private Object data;
 
-    public StrandedResponse(boolean state, String message) {
+    public StrandedResponse() {
+    }
+
+    public StrandedResponse(boolean state, String message, Object data) {
         this.state = state;
         this.message = message;
+        this.data = data;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "StrandedResponse{" +
+                "state=" + state +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
